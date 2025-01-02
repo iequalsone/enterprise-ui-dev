6c36e5debdb65dc51b743b38ddb9adef6fe03622
Jon <johnny.rtn@gmail.com>
2025-01-02 20:05:43 -0330

    Refactor test utilities to enhance rendering and user interaction capabilities

--- src/examples/counter/test/utilities.ts
+++ src/examples/counter/test/utilities.ts
@@ -1,1 +1,8 @@
-import '@testing-library/jest-dom/extend-expect';
+import { render as renderComponent } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+
+export * from '@testing-library/react';
+
+export const render = (ui: React.ReactElement, options?: Parameters<typeof renderComponent>[1]) => {
+  return { ...renderComponent(ui, options), user: userEvent.setup() }
+}
