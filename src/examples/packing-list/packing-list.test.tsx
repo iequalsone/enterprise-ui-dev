8ea9ad1b93db7de090227e6fcd4ee1fdb002c517
Jon <johnny.rtn@gmail.com>
2025-01-04 08:41:19 -0330

    Implement tests for Packing List component input field and button functionality

--- src/examples/packing-list/packing-list.test.tsx
+++ src/examples/packing-list/packing-list.test.tsx
@@ -10,16 +10,33 @@ it('has the correct title', async () => {
   screen.getByText('Packing List');
 });
 
-it.todo('has an input field for a new item', () => {});
+it('has an input field for a new item', () => {
+  render(<PackingList />)
+  screen.getByLabelText('New Item Name')
+});
 
-it.todo(
+it(
   'has a "Add New Item" button that is disabled when the input is empty',
-  () => {},
+  () => {
+    render(<PackingList />)
+    const newItemInput = screen.getByLabelText('New Item Name')
+    const addNewItemButton = screen.getByRole('button', { name: 'Add New Item' })
+
+    expect(newItemInput).toHaveValue('')
+    expect(addNewItemButton).toBeDisabled()
+  },
 );
 
-it.todo(
+it(
   'enables the "Add New Item" button when there is text in the input field',
-  async () => {},
+  async () => {
+    const { user } = render(<PackingList />)
+    const newItemInput = screen.getByLabelText('New Item Name')
+    const addNewItemButton = screen.getByRole('button', { name: 'Add New Item' })
+
+    await user.type(newItemInput, 'MacBook Pro')
+    expect(addNewItemButton).toBeEnabled();
+  },
 );
 
 it.todo(
